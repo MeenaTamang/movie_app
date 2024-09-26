457eeedc2b220056cdefe55276bf4b380bfef90f
meenabhlon <meenabhlon@gmail.com>
2024-09-26 07:33:58 +0545

    add movie screen

--- lib/app/modules/home/views/subviews/movie_view.dart
+++ lib/app/modules/home/views/subviews/movie_view.dart
@@ -1,22 +1,91 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
+import 'package:movie_app/app/core/values/s_spacing.dart';
 
 class MovieView extends GetView {
   const MovieView({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('MovieView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'MovieView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+      
+      body: Container(
+
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+
+              CarouselSlider(
+                options: CarouselOptions(height: 400.0, autoPlay: true),
+                  items: [1,2,3,4,5].map((i) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.symmetric(horizontal: 5.0),
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 160, 133, 201)
+                        ),
+                        child: Text('text $i', style: TextStyle(fontSize: 16.0),)
+                        );
+                    },
+                  );
+                  }).toList(),
+              ),
+
+              Row(
+                children: [
+                  SSpacing.smW,
+                  Text('Popular Movies',
+                  style: Get.textTheme.titleLarge,
+                
+                  ),
+                  const Spacer(),
+
+                  Text('see all',
+                  style: Get.textTheme.bodyMedium,
+                  ),
+                  Icon(Icons.arrow_forward_ios, size: 10),
+                  SSpacing.smW,
+
+                ],
+              ),
+
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      width: 150,
+                      height: 200,
+                      color: Color.fromARGB(255, 160, 133, 201),
+                    ),
+                    SSpacing.smW,
+                    Container(
+                      width: 150,
+                      height: 200,
+                      color: Color.fromARGB(255, 160, 133, 201),
+                    ),
+                    SSpacing.smW,
+                    Container(
+                      width: 150,
+                      height: 200,
+                      color: Color.fromARGB(255, 160, 133, 201),
+                    ),
+                    SSpacing.smW,
+                    Container(
+                      width: 150,
+                      height: 200,
+                      color: Color.fromARGB(255, 160, 133, 201),
+                    ),
+                    SSpacing.smW,
+                  ],
+                ),
+              )
+            ],
+          )
+          ),
+      )
     );
   }
 }
